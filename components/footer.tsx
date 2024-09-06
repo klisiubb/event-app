48064bb0cd35e017250266027793fdcb71f097df
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-09-06 13:49:43 +0200

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,12 +2,10 @@ import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <div className="flex justify-center py-4 md:py-8 h-[80px] border-t  md:ms-[224px]">
+    <div className="hidden md:flex justify-center py-4 md:py-8 h-[80px] border-t  md:ms-[224px]">
       <div className="text-sm  mr-4 flex flex-col items-center md:flex-row">
-        <span className="hidden md:block">
-          Event App &copy; {new Date().getFullYear()} |
-        </span>
-        <span className="italic mx-1">
+        <span>Event App &copy; {new Date().getFullYear()} |</span>
+        <span className="italic mx-1 hover:text-primary hover:animate-pulse">
           by <Link href="https://github.com/klisiubb/">Mateusz Kliś</Link>
         </span>
       </div>
