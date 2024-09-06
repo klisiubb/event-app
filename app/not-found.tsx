175c9ddae2712edec0e48d1df0fd091129bc41fe
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-09-06 18:05:37 +0200

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -11,7 +11,7 @@ export default function NotFound() {
   return (
     <div className="h-screen flex flex-col items-center justify-center">
       <div className="animate-shake animate-once animate-duration-[2000ms] animate-ease-in-out text-center">
-        <h1 className="text-5xl md:text-8xl font-bold text-primary animate-pulse hover:animate-shake">
+        <h1 className="text-5xl md:text-8xl font-bold text-primary animate-pulse animate-infinite hover:animate-shake">
           {" "}
           404 - Not Found
         </h1>
@@ -20,7 +20,7 @@ export default function NotFound() {
         </p>
         <Button
           asChild
-          className="w-3/4 md:w-1/3 animate-bounce hover:animate-pulse"
+          className="w-3/4 md:w-1/3 animate-bounce animate-infinite hover:animate-pulse"
         >
           <Link href="/">Go home!</Link>
         </Button>
