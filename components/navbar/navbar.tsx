56f72768d04139bb76a5915125f56d155576c84f
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-09-23 17:54:19 +0200

    Small fixes

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -3,9 +3,11 @@ import { NavbarRoutes } from "./navbar-routes";
 
 export const Navbar = () => {
   return (
-    <div className="p-4 border-b h-full flex items-center shadow-sm">
-      <MobileSidebar />
-      <NavbarRoutes />
+    <div className="fixed top-0 left-0 right-0 z-50 bg-background">
+      <div className="p-4 border-b h-16 flex items-center shadow-sm">
+        <MobileSidebar />
+        <NavbarRoutes />
+      </div>
     </div>
   );
 };
