1bd09d1bdb7db67e4d4679ddc7e04ea8c18e4dd2
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-10-08 10:52:08 +0200

    Added searching by room

--- components/admin/lecture/lectures-view.tsx
+++ components/admin/lecture/lectures-view.tsx
@@ -11,6 +11,7 @@ export const LecturesView = ({ lectures }: { lectures: Lecture[] }) => {
   const filteredLectures = useFilter<Lecture>(lectures, searchTerm, [
     "topic",
     "description",
+    "room",
   ]);
   return (
     <div className="min-h-[calc(100vh-160px)] p-6 md:p-10">
@@ -21,7 +22,7 @@ export const LecturesView = ({ lectures }: { lectures: Lecture[] }) => {
             type="search"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            placeholder="Search lectures by topic or description..."
+            placeholder="Search lectures by topic, room or description..."
             className="pl-10 pr-4 py-2 w-full rounded-full"
           />
         </div>
