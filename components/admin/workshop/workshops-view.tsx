7f8073527fd8b7f391d1f5b1a0a742bc718cf616
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-10-09 18:51:32 +0200

    Update workshops-view.tsx

--- components/admin/workshop/workshops-view.tsx
+++ components/admin/workshop/workshops-view.tsx
@@ -6,6 +6,9 @@ import { Search } from "lucide-react";
 import { useFilter } from "@/lib/use-filter";
 import WorkshopCard from "./workshop-card";
 import { WorkshopWithQRCode } from "@/types/workshop-qrcode.type";
+import CreateDialog from "../create-dialog";
+import { WorkshopFormSchema } from "@/schemas/admin/workshop";
+import { CreateWorkshop } from "@/actions/admin/workshop/create";
 
 export const WorkshopsView = ({
   workshops,
@@ -23,7 +26,7 @@ export const WorkshopsView = ({
   }
   return (
     <div className="min-h-[calc(100vh-160px)] p-6 md:p-10">
-      <div className="flex justify-center mb-16">
+      <div className="flex justify-center mb-4">
         <div className="relative w-full max-w-xl">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground" />
           <Input
@@ -34,6 +37,20 @@ export const WorkshopsView = ({
             className="pl-10 pr-4 py-2 w-full rounded-full"
           />
         </div>
+      </div>
+      <div className="mb-4 flex justify-center md:justify-start">
+        <CreateDialog
+          buttonText="Create new workshop"
+          titleText="Add new workshop"
+          underTitleText="Setup workshop topic. You'll fill the rest later."
+          formSchema={WorkshopFormSchema.pick({ topic: true })}
+          fieldName="topic"
+          route="workshop"
+          createAction={CreateWorkshop}
+          labelText="Workshop name:"
+          descriptionText="You can change it later."
+          placeholderText="e.g. 'React for beginners'"
+        />
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
         {filteredWorkshops.map((workshop) => (
