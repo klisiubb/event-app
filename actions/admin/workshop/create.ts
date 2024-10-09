ea386c71653f78df7c4f4c512b176c2216342ada
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-10-09 12:42:07 +0200

    Update create.ts

--- actions/admin/workshop/create.ts
+++ actions/admin/workshop/create.ts
@@ -5,6 +5,7 @@ import { prisma } from "@/lib/db";
 import { WorkshopFormSchema } from "@/schemas/admin/workshop";
 import { Prisma } from "@prisma/client";
 import { ZodError } from "zod";
+var QRCode = require("qrcode");
 
 export async function CreateWorkshop({
   topic,
@@ -12,13 +13,24 @@ export async function CreateWorkshop({
   topic: string;
 }): Promise<ActionReturnType> {
   let workshop;
+  let qrcode;
+  let base64;
+  let value = Number(process.env.APP_Workshop_POINTS_VALUE) || 10;
+  let maxAttenders =
+    Number(process.env.APP_WORKSHOP_MAXIMUM_PARTICIPANTS_VALUE) || 15;
+
   try {
     await WorkshopFormSchema.pick({ topic: true }).parseAsync({ topic });
 
-    workshop = await prisma.workshop.create({
+    qrcode = await prisma.qrCode.create({
       data: {
-        topic,
+        name: topic,
+        value,
       },
+    });
+
+    workshop = await prisma.workshop.create({
+      data: { topic, maxAttenders, qrcode: { connect: { id: qrcode.id } } },
     });
   } catch (e) {
     if (e instanceof ZodError) {
@@ -31,19 +43,51 @@ export async function CreateWorkshop({
       if (e.code === "P2002") {
         return {
           status: 400,
-          message: "Workshop with this topic already exist.",
+          message: "Workshop with this topic already exists.",
         };
       }
     } else {
-      console.log(e);
       return {
         status: 400,
         message: "Error. Try again later.",
       };
     }
   }
+
+  if (!workshop) {
+    return {
+      status: 500,
+      message: "Workshop creation failed.",
+    };
+  }
+
+  try {
+    base64 = await QRCode.toDataURL(workshop.id);
+
+    if (!qrcode) {
+      return {
+        status: 500,
+        message: "QR code creation failed.",
+      };
+    }
+
+    await prisma.qrCode.update({
+      where: {
+        id: qrcode.id,
+      },
+      data: {
+        base64,
+      },
+    });
+  } catch (e) {
+    return {
+      status: 500,
+      message: "Error generating QR code.",
+    };
+  }
+
   return {
-    id: workshop?.id,
+    id: workshop.id,
     status: 201,
     message: "Workshop created. Redirecting...",
   };
