56655894260af4d7b03511c30ff9d190c4057573
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-10-11 16:58:24 +0200

    Added Schema for User

---
+++ schemas/admin/user.ts
@@ -0,0 +1,28 @@
+import { Role } from "@prisma/client";
+import { z } from "zod";
+
+export const UserFormSchema = z.object({
+  firstName: z
+    .string({
+      required_error: "First name is required.",
+      invalid_type_error: "Name must be a string.",
+    })
+    .min(2, { message: "Name must be at least 2 characters long." })
+    .max(30, {
+      message: "Name must be at most 30 characters long.",
+    })
+    .trim(),
+  lastName: z
+    .string({
+      required_error: "First name is required.",
+      invalid_type_error: "Name must be a string.",
+    })
+    .min(2, { message: "Name must be at least 2 characters long." })
+    .max(30, {
+      message: "Name must be at most 30 characters long.",
+    })
+    .trim(),
+  role: z.enum([Role.ADMIN, Role.USER, Role.VOLUNTEER, Role.LECTURER]),
+});
+
+export type UserFormSchemaType = z.infer<typeof UserFormSchema>;
