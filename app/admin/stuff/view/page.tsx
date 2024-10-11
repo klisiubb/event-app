23d429d2c308fc9d39a51829b628290b65a8ad81
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-10-11 19:16:50 +0200

    Lecturer and Stuff Pages initial setup

---
+++ app/admin/stuff/view/page.tsx
@@ -0,0 +1,24 @@
+import { UsersView } from "@/components/admin/user/users-view";
+import { prisma } from "@/lib/db";
+import { Role } from "@prisma/client";
+import React from "react";
+
+const Page = async () => {
+  const users = await prisma.user.findMany({
+    where: {
+      role: Role.VOLUNTEER,
+    },
+  });
+
+  if (!users) {
+    return <></>;
+  }
+
+  return (
+    <>
+      <UsersView users={users} />
+    </>
+  );
+};
+
+export default Page;
