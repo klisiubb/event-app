54700cb803562474c3063ab85fc6c00e1dc7b415
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-10-11 17:36:44 +0200

    Add option to edit user

--- components/admin/user/user-columns.tsx
+++ components/admin/user/user-columns.tsx
@@ -1,8 +1,19 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { User } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import { ArrowUpDown } from "lucide-react";
+import { ArrowUpDown, MoreHorizontal, Trash2 } from "lucide-react";
+import Link from "next/link";
+import DeleteDialog from "../delete-dialog";
+import { DeleteUser } from "@/actions/admin/user/delete";
 
 export const columns: ColumnDef<User>[] = [
   {
@@ -29,6 +40,30 @@ export const columns: ColumnDef<User>[] = [
           Role
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "id",
+    header: "Actions",
+    cell: ({ row }) => {
+      const user = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions:</DropdownMenuLabel>
+            <DropdownMenuItem>
+              <Link href={`/admin/user/edit/${user.id}`}>Edit</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       );
     },
   },
