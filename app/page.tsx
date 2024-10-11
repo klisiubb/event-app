a151589abed32974f5d888aff79234180a3db780
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-10-11 19:00:59 +0200

    Temporary front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,40 @@
 import { Button } from "@/components/ui/button";
+import { TypingAnimation } from "@/components/ui/typing-animation";
 import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import Link from "next/link";
 import React from "react";
 
 const Page = async () => {
   const { isAuthenticated, getPermission } = getKindeServerSession();
+  const text = `Welcome to the Event App, your one-stop solution for managing and
+            attending events.`;
   const isAdmin = await getPermission("admin");
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-6xl font-bold">Event App</h1>
-      <p className="mt-2">More info soon.</p>
-      {(await isAuthenticated()) ? (
-        <>
-          <Button className="my-4">
-            <LogoutLink>Log out </LogoutLink>
-          </Button>
-          {isAdmin?.isGranted ? (
-            <Button asChild variant="outline">
-              <Link href="/admin/">Go to admin panel</Link>
-            </Button>
-          ) : (
-            <> </>
-          )}
-        </>
-      ) : (
-        <Button className="my-4">
-          <LoginLink>Log in please</LoginLink>
-        </Button>
-      )}
+    <div className="flex flex-col items-center justify-center min-h-screen  bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+      <section>
+        <div className="flex max-w-[64rem] flex-col justify-center items-center gap-4">
+          <h1 className="font-bold text-3xl sm:text-5xl md:text-6xl lg:text-7xl bg-clip-text text-transparent bg-gradient-to-tr from-primary to-destructive">
+            Event App
+          </h1>
+          <TypingAnimation
+            className="leading-normal text-muted-foreground sm:text-xl sm:leading-8 text-center"
+            text={text}
+            duration={50}
+          />
+
+          <div className="space-x-4">
+            {(await isAuthenticated()) ? (
+              <Button asChild size="lg">
+                <LogoutLink>Log out </LogoutLink>
+              </Button>
+            ) : (
+              <Button asChild size="lg">
+                <LoginLink>Join us </LoginLink>
+              </Button>
+            )}
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
