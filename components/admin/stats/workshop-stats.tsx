f18635aacef19496e10b7052e307b9ae52b5b70a
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-10-13 17:49:04 +0200

    Added missing blur effect

--- components/admin/stats/workshop-stats.tsx
+++ components/admin/stats/workshop-stats.tsx
@@ -3,36 +3,39 @@ import React from "react";
 import { OverviewItem } from "./overview-item";
 import { prisma } from "@/lib/db";
 import { Role } from "@prisma/client";
+import { BlurFade } from "@/components/ui/blur-fade";
 
 const WorkshopStats = async () => {
   const users = await prisma.user.findMany({ where: { role: Role.USER } });
   return (
-    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-      <OverviewItem
-        title="Registered users"
-        subTitle="Users that are not part of stuff or lecturers"
-        icon={UsersIcon}
-        number={users.length}
-      />
-      <OverviewItem
-        title="Registered users"
-        subTitle="Users that are not part of stuff or lecturers"
-        icon={UsersIcon}
-        number={users.length}
-      />
-      <OverviewItem
-        title="Registered users"
-        subTitle="Users that are not part of stuff or lecturers"
-        icon={UsersIcon}
-        number={users.length}
-      />
-      <OverviewItem
-        title="Registered users"
-        subTitle="Users that are not part of stuff or lecturers"
-        icon={UsersIcon}
-        number={users.length}
-      />
-    </div>
+    <BlurFade inView delay={0.1}>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+        <OverviewItem
+          title="Registered users"
+          subTitle="Users that are not part of stuff or lecturers"
+          icon={UsersIcon}
+          number={users.length}
+        />
+        <OverviewItem
+          title="Registered users"
+          subTitle="Users that are not part of stuff or lecturers"
+          icon={UsersIcon}
+          number={users.length}
+        />
+        <OverviewItem
+          title="Registered users"
+          subTitle="Users that are not part of stuff or lecturers"
+          icon={UsersIcon}
+          number={users.length}
+        />
+        <OverviewItem
+          title="Registered users"
+          subTitle="Users that are not part of stuff or lecturers"
+          icon={UsersIcon}
+          number={users.length}
+        />
+      </div>
+    </BlurFade>
   );
 };
 
