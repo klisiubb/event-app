6a2d27befb7406d8f6ff1a77f6750cf354187bdf
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-10-15 14:18:28 +0200

    Remove unused component

--- components/admin/user/user-columns.tsx
+++ components/admin/user/user-columns.tsx
@@ -1,15 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { User } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import { MoreHorizontal } from "lucide-react";
 import { Link } from "next-view-transitions";
 
 export const columns: ColumnDef<User>[] = [
@@ -35,25 +27,13 @@ export const columns: ColumnDef<User>[] = [
   },
   {
     accessorKey: "id",
-    header: "Actions",
+    header: "Edit",
     cell: ({ row }) => {
       const user = row.original;
-
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions:</DropdownMenuLabel>
-            <DropdownMenuItem>
-              <Link href={`/admin/user/edit/${user.id}`}>Edit</Link>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <Button variant="outline">
+          <Link href={`/admin/user/edit/${user.id}`}>Edit</Link>
+        </Button>
       );
     },
   },
