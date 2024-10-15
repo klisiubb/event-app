de154fd8c659d724f6f6d1f663ea4dc8cc195e7e
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-10-15 10:32:41 +0200

    Link fixed

--- components/navbar/logo.tsx
+++ components/navbar/logo.tsx
@@ -5,7 +5,7 @@ export const Logo = () => {
   return (
     <Link
       className="flex items-center hover:text-primary hover:font-bold"
-      href="/"
+      href="/admin/"
     >
       <CalendarCheck />
       <span className="ml-2 mt-1">Event App</span>
