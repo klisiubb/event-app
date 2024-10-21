036347648f5eda64d9489ec41fa9814b0e36d710
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-10-21 17:11:23 +0200

    Added Agenda page

---
+++ app/agenda/page.tsx
@@ -0,0 +1,33 @@
+import { prisma } from "@/lib/db";
+import React from "react";
+import Agenda from "@/components/lading-page/agenda";
+
+const Page = async () => {
+  const workshops = await prisma.workshop.findMany({
+    where: {
+      isPublished: true,
+    },
+    include: {
+      workshopLecturers: true,
+    },
+  });
+  const lectures = await prisma.lecture.findMany({
+    where: {
+      isPublished: true,
+    },
+    include: {
+      lectureLecturers: true,
+    },
+  });
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center gap-8 pt-24">
+      <h1 className="font-bold text-6xl md:text-7xl  bg-clip-text text-transparent bg-gradient-to-tr from-primary to-destructive pb-2 mt-16 md:mt-12">
+        Event agenda
+      </h1>
+
+      <Agenda workshops={workshops} lectures={lectures} />
+    </div>
+  );
+};
+
+export default Page;
