7eff17fea1adf33b1d72a595fa0054b73cb82f3f
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-10-22 16:47:31 +0200

    Added countdown

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Countdown from "@/components/lading-page/countdown";
 import { Button } from "@/components/ui/button";
 import { TypingAnimation } from "@/components/ui/typing-animation";
 import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
@@ -14,7 +15,7 @@ const Page = async () => {
         className="h-screen flex flex-col justify-center items-center gap-4"
         id="landing"
       >
-        <h1 className="font-bold text-6xl sm:text-6xl md:text-7xl lg:text-8xl bg-clip-text text-transparent bg-gradient-to-tr from-primary to-destructive">
+        <h1 className="font-bold text-6xl sm:text-6xl md:text-7xl lg:text-8xl bg-clip-text text-transparent bg-gradient-to-tr from-primary to-destructive md:pb-4">
           Event App
         </h1>
         <TypingAnimation
@@ -34,6 +35,15 @@ const Page = async () => {
             </Button>
           )}
         </div>
+      </section>
+      <section
+        className="flex flex-col justify-center items-center h-screen gap-4"
+        suppressHydrationWarning
+      >
+        <h1 className="font-bold text-6xl sm:text-6xl md:text-7xl lg:text-8xl bg-clip-text text-transparent bg-gradient-to-tr from-primary to-destructive md:pb-4 ">
+          Next edition is coming in:
+        </h1>
+        <Countdown targetDate={new Date("2025-05-30T06:00:00Z")} />
       </section>
     </>
   );
