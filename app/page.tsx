33afa885faced242d663cba3cfe281d39d3d98d7
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-11-21 19:17:35 +0100

    Small changes to front page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { Button } from "@/components/ui/button";
 import { TypingAnimation } from "@/components/ui/typing-animation";
 import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { Ticket } from "lucide-react";
 import { Link } from "next-view-transitions";
 import React from "react";
 
@@ -15,21 +14,21 @@ const Page = async () => {
   const eventDate = new Date("2025-05-30T06:00:00Z");
 
   return (
-    <>
+    <div className="flex flex-col">
       <section
-        className="h-screen flex flex-col justify-center items-center gap-4"
+        className="h-screen flex flex-col justify-center items-center px-4"
         id="landing"
       >
-        <h1 className="font-bold text-6xl sm:text-6xl md:text-7xl lg:text-8xl bg-clip-text text-transparent bg-gradient-to-tr from-primary to-destructive md:pb-4">
+        <h1 className="font-bold text-5xl sm:text-6xl md:text-7xl lg:text-8xl bg-clip-text text-transparent bg-gradient-to-tr from-primary to-destructive text-center mb-6">
           Event App
         </h1>
         <TypingAnimation
-          className="leading-normal text-base sm:text-lg md:text-xl lg:text-2xl text-muted-foreground text-center px-4"
+          className="leading-relaxed text-base sm:text-lg md:text-xl lg:text-2xl text-muted-foreground text-center max-w-3xl mb-8"
           text={text}
           duration={25}
         />
         <BlurFade inView duration={1}>
-          <div className="space-x-4 mt-4">
+          <div className="flex flex-wrap justify-center gap-4">
             {(await isAuthenticated()) ? (
               <Button asChild size="lg">
                 <LogoutLink>Log out</LogoutLink>
@@ -45,23 +44,28 @@ const Page = async () => {
           </div>
         </BlurFade>
       </section>
+
       <section
-        className="flex flex-col justify-center items-center h-screen gap-4"
+        className="h-screen flex flex-col justify-center items-center px-4"
         id="timer"
       >
-        <h1 className="font-bold text-center text-6xl sm:text-6xl md:text-7xl lg:text-8xl bg-clip-text text-transparent bg-gradient-to-tr from-primary to-destructive pb-10 md:pb-4">
+        <h2 className="font-bold text-center text-4xl sm:text-5xl md:text-6xl lg:text-7xl bg-clip-text text-transparent bg-gradient-to-tr from-primary to-destructive mb-10">
           Next edition is coming in:
-        </h1>
+        </h2>
         <BlurFade inView duration={1}>
           <Countdown targetDate={eventDate} />
         </BlurFade>
       </section>
-      <section className=" h-screen" id="stats">
-        <h1 className="font-bold text-center text-6xl sm:text-6xl md:text-7xl lg:text-8xl bg-clip-text text-transparent bg-gradient-to-tr from-primary to-destructive pb-10 md:pb-8">
+
+      <section
+        className="h-screen flex flex-col justify-center items-center px-4"
+        id="stats"
+      >
+        <h2 className="font-bold text-center text-4xl sm:text-5xl md:text-6xl lg:text-7xl bg-clip-text text-transparent bg-gradient-to-tr from-primary to-destructive mb-12">
           Past event editions in numbers:
-        </h1>
+        </h2>
         <BlurFade inView duration={1}>
-          <div className="mt-2 flex flex-wrap justify-center items-center gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 max-w-6xl mx-auto">
             <StatsCard name="Editions" stat={13} />
             <StatsCard name="Attenders" stat={6000} />
             <StatsCard name="Rewards given" stat={250} />
@@ -69,7 +73,7 @@ const Page = async () => {
           </div>
         </BlurFade>
       </section>
-    </>
+    </div>
   );
 };
 
