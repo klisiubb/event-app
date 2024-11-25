14b48aee71fa03bdfaf0b680bb1952b4f953568f
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-11-25 15:19:26 +0100

    Moving to Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@/components/providers/theme";
 import { Toaster } from "sonner";
 import { ViewTransitions } from "next-view-transitions";
 import Navbar from "@/components/lading-page/navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -41,21 +42,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ViewTransitions>
-      <html lang="en">
-        <body className={inter.className}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Navbar />
-            {children}
-            <Toaster richColors />
-          </ThemeProvider>
-        </body>
-      </html>
-    </ViewTransitions>
+    <ClerkProvider>
+      <ViewTransitions>
+        <html lang="en">
+          <body className={inter.className}>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <Navbar />
+              {children}
+              <Toaster richColors />
+            </ThemeProvider>
+          </body>
+        </html>
+      </ViewTransitions>
+    </ClerkProvider>
   );
 }
