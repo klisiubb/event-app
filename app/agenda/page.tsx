1872df3a7205882dd44d9b6d31b1b91af7f3d793
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-11-29 19:47:42 +0100

    Added dashboard button

--- app/agenda/page.tsx
+++ app/agenda/page.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 
 import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Link } from "next-view-transitions";
 
 export const revalidate = 30;
 export const dynamic = "force-dynamic";
@@ -51,7 +52,32 @@ const Page = async () => {
           </div>
 
           {(await isAuthenticated()) ? (
-            <></>
+            <>
+              <div className="space-y-8">
+                <div className="flex flex-col max-w-screen-xl mx-auto text-center  gap-8">
+                  <h1 className="text-4xl md:text-6xl font-bold">
+                    Go to your
+                    <span className="bg-clip-text text-transparent bg-gradient-to-tr from-primary to-destructive">
+                      {" "}
+                      user panel
+                    </span>
+                  </h1>
+                  <p className="max-w-screen-sm mx-auto text-xl text-muted-foreground ">
+                    You can check your stats, setup your profile and check your
+                    workshop.
+                  </p>
+
+                  <Button
+                    variant="secondary"
+                    size="lg"
+                    asChild
+                    className="mx-96"
+                  >
+                    <Link href="/dashboard">Go to dashboard</Link>
+                  </Button>
+                </div>
+              </div>
+            </>
           ) : (
             <>
               <div className="space-y-8">
