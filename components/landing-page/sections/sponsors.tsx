364f6ed88d55f7ee24f6ad9f677da0754b4a2350
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-12-02 10:57:13 +0100

    Changed sponsor section to display Images

--- components/landing-page/sections/sponsors.tsx
+++ components/landing-page/sections/sponsors.tsx
@@ -10,16 +10,19 @@ import {
 } from "@/components/ui/carousel";
 import { Sponsor } from "@prisma/client";
 import { Separator } from "@/components/ui/separator";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Link } from "next-view-transitions";
+
 export const SponsorsSection = ({ sponsors }: { sponsors: Sponsor[] }) => {
   return (
     <section id="sponsors" className="max-w-[75%] mx-auto pb-24 sm:pb-32">
       <h2 className="text-3xl text-primary text-center mb-4 tracking-wider font-bold uppercase">
         Our sponsors
       </h2>
-      <Separator className="bg-primary" />
 
-      <h3 className=" mx-auto text-lg text-center text-muted-foreground my-8">
-        This event couln&apos;t be possible without them. Thank you.
+      <h3 className=" mx-auto text-lg text-center text-muted-foreground my-4">
+        This event couldn&apos;t be possible without them. Thank you.
       </h3>
 
       <div className="mx-auto">
@@ -40,13 +43,29 @@ export const SponsorsSection = ({ sponsors }: { sponsors: Sponsor[] }) => {
                 className="md:basis-1/2 lg:basis-1/3 flex flex-col align-middle justify-between items-center"
                 key={sponsor.id}
               >
-                {sponsor.name}
+                <div className="w-[350px] h-[250px] flex justify-center">
+                  <Image
+                    src={sponsor.imageUrl || ""}
+                    alt={sponsor.name}
+                    width={600}
+                    height={600}
+                    className="sm:max-w-[350px] lg:max-w-[325px] max-h-[250px] max-w-[250px] object-scale-down self-center align-middle"
+                  />
+                </div>
               </CarouselItem>
             ))}
           </CarouselContent>
           <CarouselPrevious />
           <CarouselNext />
         </Carousel>
+      </div>
+      <h3 className=" mx-auto text-lg text-center text-muted-foreground my-4">
+        You can read more about our sponsors on dedicated sponsor page.
+      </h3>
+      <div className="flex justify-center pt-4">
+        <Button variant="secondary" size="lg" asChild className="mx-96">
+          <Link href="#sponsors">Take me there</Link>
+        </Button>
       </div>
     </section>
   );
