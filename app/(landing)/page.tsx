7277bfbc3dae038d10d1ab42b3cd4c09850ca542
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-12-08 18:43:30 +0100

    Added rewards to main page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,13 +2,12 @@ import { DiscordSection } from "@/components/landing-page//sections/discord";
 import { FAQSection } from "@/components/landing-page/sections/faq";
 import HeroSection from "@/components/landing-page/sections/hero";
 import NewEditionSection from "@/components/landing-page/sections/new-edition";
-
 import PercsSection from "@/components/landing-page/sections/percs";
-
 import React from "react";
 import { prisma } from "@/lib/db";
 import { SponsorsSection } from "@/components/landing-page/sections/sponsors";
 import PastEditionSection from "@/components/landing-page/sections/past-editions";
+import { RewardsSection } from "@/components/landing-page/sections/rewards";
 
 export const revalidate = 30;
 export const dynamic = "force-dynamic";
@@ -19,11 +18,19 @@ const Page = async () => {
       isPublished: true,
     },
   });
+  const rewards = await prisma.reward.findMany({
+    where: {
+      isPublished: true,
+    },
+    take: 4,
+  });
+
   return (
     <>
       <HeroSection />
       <NewEditionSection />
       <PercsSection />
+      <RewardsSection rewards={rewards} />
       <PastEditionSection />
       <SponsorsSection sponsors={sponsors} />
       <DiscordSection />
