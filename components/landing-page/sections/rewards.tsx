ce1dafaff489196687045478a3fefdcda56cf285
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-12-08 18:53:36 +0100

    Fixing cards

--- components/landing-page/sections/rewards.tsx
+++ components/landing-page/sections/rewards.tsx
@@ -1,25 +1,27 @@
 import { Reward } from "@prisma/client";
-import { RewardCard } from "@/components/landing-page//reward-card";
+import { RewardCard } from "@/components/landing-page/reward-card";
+
 export const RewardsSection = async ({ rewards }: { rewards: Reward[] }) => {
   return (
-    <section id="rewards" className="container lg:w-[75%] py-16">
+    <section id="rewards" className="container lg:w-[90%] xl:w-[80%] pb-16">
       <div className="text-center">
         <h2 className="text-2xl sm:text-3xl text-primary mb-2 sm:mb-4 tracking-wider font-bold uppercase mt-8">
           Check out our rewards
         </h2>
 
-        <h3 className="md:w-1/2 mx-auto text-base sm:text-lg text-muted-foreground mb-8">
+        <h3 className="md:w-2/3 lg:w-1/2 mx-auto text-base sm:text-lg text-muted-foreground mb-8">
           We respect your time. Scan the QR codes for a chance to win awesome
           rewards.
         </h3>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-12">
         {rewards.map((reward) => (
           <RewardCard key={reward.id} reward={reward} />
         ))}
       </div>
-      <h3 className="md:w-1/2 mx-auto text-base sm:text-lg text-muted-foreground mt-8 text-center">
+
+      <h3 className="md:w-2/3 lg:w-1/2 mx-auto text-base sm:text-lg text-muted-foreground mt-8 text-center">
         That&apos;s not all! You can find more rewards in our mobile app or by
         visiting sponsors booth.
       </h3>
